7a863edba0c39d50df7dcaeef5faf3f61a1a27c7
Alice Freda <alicefreda@gmail.com>
2020-05-07 20:37:05 -0700

    add functions for start, draw, and stop

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,25 @@ canvas.addEventListener('mousedown', start);
 canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mouseup', stop);
 
+function start (e) {
+    isDrawing = true;
+}
+
+function draw ({clientX: x, clientY: y}) {
+    if (!isDrawing) return; // if not drawing, do nothing
+    ctx.lineWidth = 3;
+    ctx.lineCap = "round";
+    ctx.strokeStyle = "#171717";
+
+    // DEV ED tutorial: https://www.youtube.com/c/dev_ed
+    ctx.lineTo(x, y);
+    ctx.stroke();
+}
+
+function stop () {
+    isDrawing = false;
+}
+
 window.addEventListener('resize', resizeCanvas);
 function resizeCanvas () {
     canvas.width = window.innerWidth;
