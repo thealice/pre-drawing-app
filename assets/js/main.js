fd31bf84149b4df9cdd6900f69bc031faeed1fb1
Alice Freda <alicefreda@gmail.com>
2020-05-07 20:30:39 -0700

    add event listeners for mousedown, mousemove and mouseup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,10 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
+let isDrawing = false;
+
+canvas.addEventListener('mousedown', start);
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', stop);
 
 window.addEventListener('resize', resizeCanvas);
 function resizeCanvas () {
