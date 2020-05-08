26e1e8a81315f3e376b328118eb9fc37b3707375
Alice Freda <alicefreda@gmail.com>
2020-05-07 20:26:02 -0700

    resize canvas to be height and width of widow

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-console.log("Hello, Wordl!")
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+window.addEventListener('resize', resizeCanvas);
+function resizeCanvas () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+resizeCanvas();
